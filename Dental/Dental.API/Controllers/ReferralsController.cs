4cbb88d1847766b4cb870fdc229fd7cdf8521ec4
Grish1nds <Grish1nds@yandex.ru>
2015-11-13 15:59:50 +0300

    add code for copy referrals //it doesn't work now

--- Dental/Dental.API/Controllers/ReferralsController.cs
+++ Dental/Dental.API/Controllers/ReferralsController.cs
@@ -6,20 +6,57 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Newtonsoft.Json;
+using Dental.DataLayer.Data;
+using Dental.API.Models;
 
 namespace Dental.API.Controllers
 {
     public class ReferralsController : ApiController
     {
+        [HttpPost]
+        public async Task<object> GetReferredByPatientID()
+        {
+            string jsonPersontID = await Request.Content.ReadAsStringAsync();
+
+            string personID = JsonConvert.DeserializeObject<string>(jsonPersontID);
+
+            DentalContext context = new DentalContext();
+
+            var person = context.People.FirstOrDefault(x => x.ID == personID);
+            if (person != null)
+            {
+                return person.referedById;
+            }
+
+            return -1;
+        }
 
         [HttpPost]
-        public async Task<object> GetReferralsByAccountID()
+        public async Task<object> GetTreatmentWithStatus2ByPatientId()
         {
-            string jsonAccountID = await Request.Content.ReadAsStringAsync();
+            string jsonPersontID = await Request.Content.ReadAsStringAsync();
 
-            int accountID = Convert.ToInt32(JsonConvert.DeserializeObject<string>(jsonAccountID));
+            string personID = JsonConvert.DeserializeObject<string>(jsonPersontID);
 
-            return 1;
+            DentalContext context = new DentalContext();
+
+            var treatment = context.Treatments.Where(x => x.patientId == personID && x.status == 2).ToList();
+            List<LpiDentalProcedure> lpiDentalProcedure = new List<LpiDentalProcedure>();
+            foreach (var item in treatment)
+            {
+                lpiDentalProcedure.Add(new LpiDentalProcedure()
+                {
+                    amount = (double)item.amount,
+                    apptId = "0",//? Grish1nds TEST
+                    code = item.stdTrtId,
+                    dateTime = item.date,
+                    DB = item.DataBaseNumber,
+                    ID = item.ID,
+                    patientId = item.patientId,
+                    TreatmentPlan = false//? Grish1nds TEST
+                });
+            }
+            return lpiDentalProcedure;
         }
 
         //public int Test()
@@ -29,6 +66,32 @@ namespace Dental.API.Controllers
 
         //    return 27;
         //}
+
+
+
+
+
+
+        ////Old Code
+        //[HttpPost]
+        //public async Task<object> GetReferralsByAccountID()
+        //{
+        //    string jsonAccountID = await Request.Content.ReadAsStringAsync();
+
+        //    string accountID =JsonConvert.DeserializeObject<string>(jsonAccountID);
+
+        //    DentalContext context =new DentalContext();
+
+        //    var person = context.People.FirstOrDefault(x => x.ID == accountID);
+        //    if (person != null)
+        //    {
+        //        return person.referedById;
+        //    }
+
+        //    return -1;
+        //}
+
+
     }
 }
 
--- LPI/LPI.Updater/Program.cs
+++ LPI/LPI.Updater/Program.cs
@@ -11,12 +11,15 @@ using System.Net.Mail;
 using System.Threading.Tasks;
 using LPI.DataBase.Data;
 using NLog;
+using Newtonsoft.Json;
 
 namespace LPI.Updater
 {
     class Program
     {
         private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        private static List<string> errorIds = new List<string>();
 
         static void Main(string[] args)
         {
@@ -28,21 +31,19 @@ namespace LPI.Updater
             {
                 logger.Error(ex);
             }
-
         }
-
+        
         public static void UpdateReferrals()
         {
             LPIContext context = new LPIContext();
 
-            List<Account> accounts = context.Accounts.ToList();
+            List<Patient> patients = context.Patients.ToList();
 
-            RunAsync(accounts).Wait();
+            RunAsync(patients, context).Wait();
         }
 
-        static async Task RunAsync(List<Account> accounts)
+        static async Task RunAsync(List<Patient> patients, LPIContext context)
         {
-
             using (var client = new HttpClient())
             {
                 client.BaseAddress = new Uri(ConfigurationManager.AppSettings["DentalAPIBaseAddress"]);
@@ -50,16 +51,69 @@ namespace LPI.Updater
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 client.DefaultRequestHeaders.Add("apikey", ConfigurationManager.AppSettings["DentalAPIKey"]);
 
-                foreach (Account account in accounts)
+                Console.WriteLine("Upadate {0} Patients", patients.Count);
+                int index = 0;
+                foreach (Patient patient in patients)
                 {
-                    HttpResponseMessage response = await client.PostAsJsonAsync("api/referrals/GetReferralsByAccountID", account.ID.ToString());
+                    HttpResponseMessage response = await client.PostAsJsonAsync("api/referrals/GetReferredByPatientID", "3913"); //patient.ID Grish1nds TEST
 
                     if (response.IsSuccessStatusCode)
                     {
+                        var referredById = response.RequestMessage.Content.ReadAsAsync<string>().Result;
+                        patient.ReferredBy = referredById;
 
+                        response = await client.PostAsJsonAsync("api/referrals/GetTreatmentWithStatus2ByPatientId", "3913"); //patient.ID Grish1nds TEST
+                        var dentalProcedureToAdd = response.RequestMessage.Content.ReadAsAsync<List<DentalProcedure>>().Result;
+                        foreach (var dentalProcedure in dentalProcedureToAdd)
+                        {
+                            context.AddToDentalProcedures(dentalProcedure);
+                        }
+                        Console.WriteLine("#{0} PatientID {1} : success", index, patient.ID);
                     }
+                    else
+                    {
+                        errorIds.Add(patient.ID);
+                        Console.WriteLine("#{0} PatientID {1} : error", index, patient.ID);
+                    }
+
+                    index++;
                 }
             }
+            //context.SaveChanges();
         }
+
+
+
+        ////Old Code
+        //public static void UpdateReferrals()
+        //{
+        //    LPIContext context = new LPIContext();
+
+        //    List<Account> accounts = context.Accounts.ToList();
+
+        //    RunAsync(accounts).Wait();
+        //}
+
+        //static async Task RunAsync(List<Account> accounts)
+        //{
+
+        //    using (var client = new HttpClient())
+        //    {
+        //        client.BaseAddress = new Uri(ConfigurationManager.AppSettings["DentalAPIBaseAddress"]);
+        //        client.DefaultRequestHeaders.Accept.Clear();
+        //        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        //        client.DefaultRequestHeaders.Add("apikey", ConfigurationManager.AppSettings["DentalAPIKey"]);
+
+        //        foreach (Account account in accounts)
+        //        {
+        //            HttpResponseMessage response = await client.PostAsJsonAsync("api/referrals/GetReferralsByAccountID", account.ID.ToString());
+
+        //            if (response.IsSuccessStatusCode)
+        //            {
+
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
