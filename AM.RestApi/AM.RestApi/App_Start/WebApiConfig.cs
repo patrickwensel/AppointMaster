9256009b26420dbeb9631041c90f37ace473ad7d
patrickwensel <pwensel@hotmail.com>
2016-02-19 12:44:42 -0500

    API Updates
    API Updates

--- AM.RestApi/AM.RestApi/App_Start/WebApiConfig.cs
+++ AM.RestApi/AM.RestApi/App_Start/WebApiConfig.cs
@@ -16,9 +16,15 @@ namespace AM.RestApi
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/v{version}/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
         }
     }
 }
---
+++ AM.RestApi/AM.RestApi/Identities/ClinicPrincipal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+
+namespace AM.RestApi.Identities
+{
+    public class ClinicPrincipal : IPrincipal
+    {
+        public ClinicPrincipal(IIdentity identity)
+        {
+            this.Identity = identity;
+        }
+
+        public IIdentity Identity { get; }
+        public int ClinicID { get; set; }
+
+        public bool IsInRole(string role)
+        {
+            return true;
+        }
+
+        
+    }
+}
