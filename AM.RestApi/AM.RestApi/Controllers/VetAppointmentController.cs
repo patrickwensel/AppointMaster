08039cacdc0f9465107bcdf61881ee7031106b96
patrickwensel <pwensel@hotmail.com>
2016-02-24 21:05:50 -0500

    Updated
    Updated

--- AM.RestApi/AM.RestApi/Controllers/VetAppointmentController.cs
+++ AM.RestApi/AM.RestApi/Controllers/VetAppointmentController.cs
@@ -10,31 +10,32 @@ namespace AM.RestApi.Controllers
 	[RoutePrefix("api/v1/VetAppointment")]
 	public class VetAppointmentController : ApiController
     {
-        // GET: api/VetAppointment
-        public IEnumerable<string> Get()
+		// GET: api/VetAppointment
+		[Route("")]
+		public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET: api/VetAppointment/5
-        public string Get(int id)
-        {
-            return "value";
-        }
+        //// GET: api/VetAppointment/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
 
-        // POST: api/VetAppointment
-        public void Post([FromBody]string value)
-        {
-        }
+        //// POST: api/VetAppointment
+        //public void Post([FromBody]string value)
+        //{
+        //}
 
-        // PUT: api/VetAppointment/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+        //// PUT: api/VetAppointment/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
 
-        // DELETE: api/VetAppointment/5
-        public void Delete(int id)
-        {
-        }
+        //// DELETE: api/VetAppointment/5
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
