ccd88778a47a2b1983503f6a52e48c4c9959bccd
hzhighview <haozhang@highviewsoftware.com>
2016-03-18 13:42:04 +0800

    api

--- AM.RestApi/AM.RestApi/Controllers/TestController.cs
+++ AM.RestApi/AM.RestApi/Controllers/TestController.cs
@@ -25,40 +25,40 @@ namespace AM.RestApi.Controllers
         [Route("{appointmentCode}")]
         public object Get(string appointmentCode)
         {
-            var appointment = context.Appointments.Where(x => x.Code.ToLower() == appointmentCode.ToLower()).Select(x =>
-                new AppointmentModel
-                {
-                    ID = x.ID,
-                    ClinicID = x.ClinicID,
-                    Time = x.Time,
-                    ClientID = x.ClientID,
-                    CheckedIn = x.CheckedIn.Value,
-                    Client = new ClientModel
-                    {
-                        Title = x.Client.Title,
-                        FirstName = x.Client.FirstName,
-                        LastName = x.Client.LastName,
-                        Address = x.Client.Address,
-                        City = x.Client.City,
-                        StateProvince = x.Client.StateProvince,
-                        PostalCode = x.Client.PostalCode,
-                        Phone = x.Client.Phone,
-                        Email = x.Client.Email,
-                        DefaultCultureCode = x.Client.DefaultCultureCode
-                    },
-                    Patients = x.AppointmentPatients.Select(m => new PatientModel
-                    {
-                        ID = m.ID,
-                        ClientID = m.Patient.ID,
-                        SpeciesID = m.Patient.SpeciesID,
-                        Name = m.Patient.Name,
-                        Breed = m.Patient.Breed,
-                        Birthdate = m.Patient.Birthdate,
-                        Gender = m.Patient.Gender
+            var appointment = context.Appointments.Where(x => x.Code.ToLower() == appointmentCode.ToLower() && x.CheckedIn == false).Select(x =>
+                  new AppointmentModel
+                  {
+                      ID = x.ID,
+                      ClinicID = x.ClinicID,
+                      Time = x.Time,
+                      ClientID = x.ClientID,
+                      CheckedIn = x.CheckedIn.Value,
+                      Client = new ClientModel
+                      {
+                          Title = x.Client.Title,
+                          FirstName = x.Client.FirstName,
+                          LastName = x.Client.LastName,
+                          Address = x.Client.Address,
+                          City = x.Client.City,
+                          StateProvince = x.Client.StateProvince,
+                          PostalCode = x.Client.PostalCode,
+                          Phone = x.Client.Phone,
+                          Email = x.Client.Email,
+                          DefaultCultureCode = x.Client.DefaultCultureCode
+                      },
+                      Patients = x.AppointmentPatients.Select(m => new PatientModel
+                      {
+                          ID = m.ID,
+                          ClientID = m.Patient.ID,
+                          SpeciesID = m.Patient.SpeciesID,
+                          Name = m.Patient.Name,
+                          Breed = m.Patient.Breed,
+                          Birthdate = m.Patient.Birthdate,
+                          Gender = m.Patient.Gender
 
-                    }).ToList()
+                      }).ToList()
 
-                }).FirstOrDefault();
+                  }).FirstOrDefault();
 
             return appointment;
         }
